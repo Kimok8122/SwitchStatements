1c12a10c18a6edf8028f99cfd6b4c0b0ef2f6642
Mona Lisa <email@example.com>
2020-08-25 13:51:09 -0600

    merge

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,39 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi! Do you have a favorite subject in school?");
+
+            string schoolSubject = Console.ReadLine();
+
+            // asking for user fave subject
+
+            switch (schoolSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("That can add up!");
+                    break;
+                case "science":
+                    Console.WriteLine("That can be a blast!");
+                    break;
+                case "history":
+                    Console.WriteLine("That will be around for awhile!");
+                    break;
+                case "lunch":
+                    Console.WriteLine("YASSSSSSS!");
+                    break;
+                case "english":
+                    Console.WriteLine("To Be or Not To Be!");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Sounds like a good one!");
+                    break;
+
+            }
+            
+            Console.WriteLine("Well, That was FUN!!!");
+
         }
     }
 }
